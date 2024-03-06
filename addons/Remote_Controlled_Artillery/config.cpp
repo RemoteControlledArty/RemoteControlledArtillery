9a878f26c2d43abc336824f855f8a81bc208e4f0
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2024-03-06 18:32:22 +0100

    test

--- addons/Remote_Controlled_Artillery/config.cpp
+++ addons/Remote_Controlled_Artillery/config.cpp
@@ -302,15 +302,14 @@ class CfgFunctions
 		class RC_Artillery
 		{
 			file="\Remote_Controlled_Artillery\functions";
-      		class preInit { preInit = 1 };
-      		class InitCBASettings { preInit = 1 };
-			class RC_MarkerHandler { postInit = 1 };
-      		class RC_UI_Loop { postInit = 1 };
-			class RC_EngineOff { postInit = 1 };
-			class RC_UAVBlur { postInit = 1 };
-			class RC_LockSeats { postInit = 1 };
-			class RC_UAVConnectionFix { postInit = 1 };
-			class RC_LaserDatalink { postInit = 1 };
+      		class InitCBASettings {preInit=1};
+      		class RC_UI_Loop {postInit=1};
+			class RC_MarkerLoop {postInit=1};
+			class RC_EngineOff {postInit=1};
+			class RC_UAVBlur {postInit=1};
+			class RC_LockSeats {postInit=1};
+			class RC_UAVConnectionFix {postInit=1};
+			class RC_LaserDatalink {postInit=1};
 			class scrollSolutions {};
 		};
 	};
