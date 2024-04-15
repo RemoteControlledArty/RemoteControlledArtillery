5e8cdc08e5bdf75727c774e9ca2653f543e370c8
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2024-04-15 16:37:18 +0200

    bla

--- addons/Remote_Controlled_Artillery/cfgVehicles/Howitzer.hpp
+++ addons/Remote_Controlled_Artillery/cfgVehicles/Howitzer.hpp
@@ -172,7 +172,7 @@ class RC_Howitzer_A: RC_Howitzer_base
 				"RC_4Rnd_155mm_Mo_HEAB",
 				"RC_5Rnd_155mm_Mo_MultiGuided",
 				"RC_2Rnd_155mm_Mo_Cluster",
-				"RC_16Rnd_155mm_Mo_smoke",
+				"RC_18Rnd_155mm_Mo_smoke",
 				"RC_9Rnd_155mm_Mo_AT_mine",
 				"RC_9Rnd_155mm_Mo_mine",
 				"RC_6Rnd_155mm_Mo_Illum"
@@ -479,7 +479,7 @@ class RC_Howitzer_HEX_A_O: RC_Howitzer_HEX_O_base
 				"RC_4Rnd_155mm_Mo_HEAB",
 				"RC_5Rnd_155mm_Mo_MultiGuided",
 				"RC_2Rnd_155mm_Mo_Cluster",
-				"RC_16Rnd_155mm_Mo_smoke",
+				"RC_18Rnd_155mm_Mo_smoke",
 				"RC_9Rnd_155mm_Mo_AT_mine",
 				"RC_9Rnd_155mm_Mo_mine",
 				"RC_6Rnd_155mm_Mo_Illum"
@@ -645,7 +645,7 @@ class RC_Static_Arty: RC_Static_Arty_base
 				"RC_4Rnd_120mm_Mo_HEAB",
 				"RC_5Rnd_120mm_Mo_MultiGuided",
 				"RC_2Rnd_120mm_Mo_Cluster",
-				"RC_16Rnd_120mm_Mo_smoke",
+				"RC_18Rnd_120mm_Mo_smoke",
 				"RC_9Rnd_120mm_Mo_AT_mine",
 				"RC_9Rnd_120mm_Mo_mine",
 				"RC_6Rnd_120mm_Mo_Illum"
