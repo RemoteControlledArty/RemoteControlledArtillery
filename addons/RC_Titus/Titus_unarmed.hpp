e8b06f52db34d7dacb92b9e81b092eb2de969376
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2024-05-11 19:59:20 +0200

    further titus development

--- addons/RC_Titus/Titus_unarmed.hpp
+++ addons/RC_Titus/Titus_unarmed.hpp
@@ -16,21 +16,27 @@ class RC_Titus_unarmed_base: QIN_Titus_DES
 	class Door_rear;
 	class comp_camonet;
 	class comp_klec;
+	class HitPoints;
+	class HitLFWheel;
+	class HitLF2Wheel;
+	class HitLMWheel;
+	class HitLBWheel;
+	class HitRFWheel;
+	class HitRF2Wheel;
+	class HitRMWheel;
+	class HitRBWheel;
 	class EventHandlers;
 	scope=0;
 	scopeCurator=0;
 	RC_Local=1; //1 = requires transfer of locality/ownership for full functionality
 };
-class RC_Titus_unarmed_D: RC_Titus_unarmed_base
+class RC_Titus_unarmed_D_base: RC_Titus_unarmed_base
 {
 	author="Ascent";
 	faction="RemoteControlled_B";
 	displayName="RC Titus";
 	editorSubcategory="RC_Titus_subcat";
-	scope=2;
-	scopeCurator=2;
-	//side=0;
-	forceInGarage=1;
+	side=0;
 	vehicleClass="Autonomous";
 	uavCameraDriverPos="PiP0_pos";
 	uavCameraDriverDir="PiP0_dir";
@@ -42,88 +48,168 @@ class RC_Titus_unarmed_D: RC_Titus_unarmed_base
 	driverForceOptics=1;
 	commanding=1;
 	//memoryPointDriverOptics="optika_velitel";
-	//driverCompartments="Compartment3";
-	//commanding=2;
+	driverCompartments="Compartment3";
+	ejectDeadGunner=0;
+	ejectDeadDriver=0;
+	ejectDeadCommander=0;
 	crewCrashProtection=0.01;
-
-	uavCameraGunnerPos="PiP1_pos";
-	uavCameraGunnerDir="PiP1_dir";
-
-	class EventHandlers: EventHandlers
+	radartype=2;
+	receiveRemoteTargets=1;
+	reportRemoteTargets=1;
+	laserScanner=1;
+	lockDetectionSystem=4;
+	incomingMissileDetectionSystem=16;
+	soundLocked[]=
 	{
-		/*
-		//init="(_this select 0) spawn {waitUntil {!isNull commander _this}; _this deleteVehicleCrew commander _this;};";
-		#include "\Remote_Controlled_Artillery\includes\DriverControlsEH_ICV.hpp"
+		"\A3\Sounds_F\weapons\Rockets\locked_1",
+		1,
+		1
+	};
+	soundIncommingMissile[]=
+	{
+		"\A3\Sounds_F\vehicles\air\noises\alarm_locked_by_missile_4",
+		0.39810717,
+		1
+	};
+	enginePower=460;
+	peakTorque=1312.5;
+	maxSpeed=120;
+	normalSpeedForwardCoef=0.64;
 
-		if (crew _myVehicle findIf { _myVehicle getCargoIndex _x == 11 } > -1) then {
-  		...
-		};
-
-		getin=
-		"params ['_vehicle']; \
-		if (isPlayer (commander _vehicle)) then { \
-			(group (driver _vehicle)) setGroupOwner (owner (commander _vehicle)); \
-			_vehicle setOwner (owner (commander _vehicle)); \
-			_vehicle setEffectiveCommander (commander _vehicle); \
-		} else { \
-			if (isPlayer (gunner _vehicle)) then { \
-				(group (driver _vehicle)) setGroupOwner (owner (gunner _vehicle)); \
-				_vehicle setOwner (owner (gunner _vehicle)); \
-				_vehicle setEffectiveCommander (gunner _vehicle); \
-			} \
-		};";
-		*/
+	smokeLauncherOnTurret=1;
+	smokeLauncherGrenadeCount=12;
+	smokeLauncherVelocity=14;
+	smokeLauncherAngle=180;
+	weapons[]=
+	{
+		"TruckHorn2",
+		"SmokeLauncher"
+	};
+	magazines[]=
+	{
+		"SmokeLauncherMag",
+		"SmokeLauncherMag"
 	};
 
-	//uavCameraGunnerPos="PiP1_pos";
-	//uavCameraGunnerDir="PiP1_dir";
-
-	class Turrets: Turrets
+	class Components: Components
 	{
-		class CommanderTurret : CommanderTurret
+		class SensorsManagerComponent
 		{
-			//gunnerCompartments="Compartment4";
-			primaryGunner=1;
-			primaryObserver=0;
-			//gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
-			//personTurretAction="";	//no effect
-			//forceHideGunner=1;	//makes view bug
-			//forceHideCommander=1;	//makes view bug
-			//gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
-			//turretInfoType="RscWeaponRangeFinder";
-			//turretInfoType="";
-			gunnerForceOptics=1;
-			//commanding=3;
+			class Components
+			{
+				class LaserSensorComponent: SensorTemplateLaser
+				{
+					class AirTarget
+					{
+						minRange=4000;
+						maxRange=4000;
+						objectDistanceLimitCoef=-1;
+						viewDistanceLimitCoef=-1;
+					};
+					class GroundTarget
+					{
+						minRange=4000;
+						maxRange=4000;
+						objectDistanceLimitCoef=-1;
+						viewDistanceLimitCoef=-1;
+					};
+				};
+				class DataLinkSensorComponent: SensorTemplateDataLink
+				{
+					typeRecognitionDistance=4000;
 
-			gunnerAction="";
-			//canHideGunner=1;
-			//viewGunnerInExternal=1;
-			isPersonTurret=0;
-			//animationSourceHatch="hatch_3";
-			personTurretAction="";
-		};
+					class AirTarget
+					{
+						minRange=4000;
+						maxRange=4000;
+						objectDistanceLimitCoef=-1;
+						viewDistanceLimitCoef=-1;
+					};
+					class GroundTarget
+					{
+						minRange=4000;
+						maxRange=4000;
+						objectDistanceLimitCoef=-1;
+						viewDistanceLimitCoef=-1;
+					};
+				};
 
-		class CargoTurret_01: CargoTurret_01 {};
-		class CargoTurret_02: CargoTurret_02 {};
-		class CargoTurret_03: CargoTurret_03
-		{
-			commanding=4;
+				#include "\Remote_Controlled_Artillery\includes\passiveRadar.hpp"
+			};
 		};
-		
-		/*
-		//seem to be blocked?
-		class CargoTurret_03: CargoTurret_01
+	};
+
+	class VehicleSystemsDisplayManagerComponentRight: DefaultVehicleSystemsDisplayManagerRight
+	{
+		defaultDisplay="SensorDisplay";
+
+		class Components
 		{
-			gunnerAction="vehicle_turnout_2";
-			memoryPointsGetInGunner="pos codriver";
-			memoryPointsGetInGunnerDir="pos codriver dir";
-			gunnerName="Co-Driver";
-			proxyIndex=11;
-			gunnerInAction="passenger_low01";
-			animationSourceHatch="hatch_4";
-			gunnerDoor="door_rf";
+			class SensorDisplay
+			{
+				componentType="SensorsDisplayComponent";
+				range[]={4000,2000,1000,500};
+				resource="RscCustomInfoSensors";
+			};
+			class MinimapDisplay
+			{
+				componentType="MinimapDisplayComponent";
+				resource="RscCustomInfoMiniMap";
+			};
+			class VehicleDriverDisplay
+			{
+				componentType="TransportFeedDisplayComponent";
+				source="Driver";
+			};
+			class EmptyDisplay
+			{
+				componentType="EmptyDisplayComponent";
+			};
+			/*
+			class MineDetectorDisplay
+			{
+				componentType="MineDetectorDisplayComponent";
+				range=50;
+				resource="RscCustomInfoMineDetect";
+			};
+			*/
 		};
-		*/
+	};
+	class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
+	{
+		defaultDisplay="VehicleDriverDisplay";
+
+		class Components
+		{
+			class SensorDisplay
+			{
+				componentType="SensorsDisplayComponent";
+				range[]={4000,2000,1000,500};
+				resource="RscCustomInfoSensors";
+			};
+			class MinimapDisplay
+			{
+				componentType="MinimapDisplayComponent";
+				resource="RscCustomInfoMiniMap";
+			};
+			class VehicleDriverDisplay
+			{
+				componentType="TransportFeedDisplayComponent";
+				source="Driver";
+			};
+			class EmptyDisplay
+			{
+				componentType="EmptyDisplayComponent";
+			};
+			/*
+			class MineDetectorDisplay
+			{
+				componentType="MineDetectorDisplayComponent";
+				range=50;
+				resource="RscCustomInfoMineDetect";
+			};
+			*/
+		};
 	};
 
 	class AnimationSources: AnimationSources
@@ -156,6 +242,245 @@ class RC_Titus_unarmed_D: RC_Titus_unarmed_base
 		0,
 		"comp_camonet",
 		0
+	};
+
+	class HitPoints: HitPoints
+	{
+		class HitLFWheel: HitLFWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitLF2Wheel: HitLF2Wheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitLMWheel: HitLMWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitLBWheel: HitLBWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitRFWheel: HitRFWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitRF2Wheel: HitRF2Wheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitRMWheel: HitRMWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+		class HitRBWheel: HitRBWheel
+		{
+			armor=-500;
+			minimalHit=-0.016000001;
+			explosionShielding=2;
+			passThrough=0;
+		};
+	};
+};
+
+class RC_Titus_unarmed_D: RC_Titus_unarmed_D_base
+{
+	class EventHandlers: EventHandlers
+	{
+		#include "\Remote_Controlled_Artillery\includes\DriverControlsEH_ICV.hpp"
+	};
+
+	scope=2;
+	scopeCurator=2;
+	forceInGarage=1;
+	uavCameraGunnerPos="PiP1_pos";
+	uavCameraGunnerDir="PiP1_dir";
+	threat[]={0.30000001,0.30000001,0.30000001};
+
+	class Turrets: Turrets
+	{
+		class CommanderTurret : CommanderTurret
+		{
+			#include "\Remote_Controlled_Artillery\includes\cfgTakeControls.hpp"
+			showAllTargets="2 + 4";
+			gunnerCompartments="Compartment4";
+			primaryGunner=1;
+			primaryObserver=0;
+			//gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
+			//personTurretAction="";	//no effect
+			//forceHideGunner=1;	//makes view bug
+			//forceHideCommander=1;	//makes view bug
+			//gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
+			//turretInfoType="RscWeaponRangeFinder";
+			//turretInfoType="";
+			gunnerForceOptics=1;
+			//commanding=3;
+
+
+			gunnerAction="";
+			//canHideGunner=1;
+			//viewGunnerInExternal=1;
+			isPersonTurret=0;
+			//animationSourceHatch="hatch_3";
+			personTurretAction="";
+
+			class ViewOptics: ViewOptics
+			{
+				initFov=1;
+				minFov=0.05;
+				maxFov=1;
+				visionMode[]=
+				{
+					"Normal",
+					"NVG",
+					"Ti"
+				};
+				thermalMode[]={0};
+			};
+
+			weapons[]=
+			{
+				"RC_Laserdesignator_vehicle",
+				"SmokeLauncher"
+			};
+			magazines[]=
+			{
+				"Laserbatteries",
+				"SmokeLauncherMag",
+				"SmokeLauncherMag"
+			};
+			
+			class Components: Components
+			{
+				class VehicleSystemsDisplayManagerComponentRight: DefaultVehicleSystemsDisplayManagerRight
+				{
+					defaultDisplay="SensorsDisplayComponent";
+
+					class Components
+					{
+						class EmptyDisplay
+						{
+							componentType="EmptyDisplayComponent";
+						};
+						class MinimapDisplay
+						{
+							componentType="MinimapDisplayComponent";
+							resource="RscCustomInfoMiniMap";
+						};
+						class UAVFeedDisplay
+						{
+							componentType="UAVFeedDisplayComponent";
+						};
+						class SensorDisplay
+						{
+							componentType="SensorsDisplayComponent";
+							range[]={4000,2000,1000,500};
+							resource="RscCustomInfoSensors";
+						};
+						/*
+						class MineDetectorDisplay
+						{
+							componentType="MineDetectorDisplayComponent";
+							range=50;
+							resource="RscCustomInfoMineDetect";
+						};
+						*/
+					};
+				};
+				class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
+				{
+					defaultDisplay="EmptyDisplayComponent";
+
+					class Components
+					{
+						class EmptyDisplay
+						{
+							componentType="EmptyDisplayComponent";
+						};
+						class MinimapDisplay
+						{
+							componentType="MinimapDisplayComponent";
+							resource="RscCustomInfoMiniMap";
+						};
+						class UAVFeedDisplay
+						{
+							componentType="UAVFeedDisplayComponent";
+						};
+						class SensorDisplay
+						{
+							componentType="SensorsDisplayComponent";
+							range[]={4000,2000,1000,500};
+							resource="RscCustomInfoSensors";
+						};
+						/*
+						class MineDetectorDisplay
+						{
+							componentType="MineDetectorDisplayComponent";
+							range=50;
+							resource="RscCustomInfoMineDetect";
+						};
+						*/
+					};
+				};
+			};
+		};
+
+		class CargoTurret_01: CargoTurret_01 {};
+		class CargoTurret_02: CargoTurret_02 {};
+		class CargoTurret_03: CargoTurret_03
+		{
+			commanding=4;
+			primaryGunner=0;
+			primaryObserver=1;
+
+			weapons[]=
+			{
+				"TruckHorn2",
+				"SmokeLauncher"
+			};
+			magazines[]=
+			{
+				"SmokeLauncherMag",
+				"SmokeLauncherMag"
+			};
+		};
+		
+		/*
+		//seem to be blocked?
+		class CargoTurret_03: CargoTurret_01
+		{
+			gunnerAction="vehicle_turnout_2";
+			memoryPointsGetInGunner="pos codriver";
+			memoryPointsGetInGunnerDir="pos codriver dir";
+			gunnerName="Co-Driver";
+			proxyIndex=11;
+			gunnerInAction="passenger_low01";
+			animationSourceHatch="hatch_4";
+			gunnerDoor="door_rf";
+		};
+		*/
 	};
 };
 
@@ -188,119 +513,29 @@ class RC_Titus_unarmed_A: RC_Titus_unarmed_D
 	};
 };
 
+/*
 class QIN_Titus_arx20_DES;
 class RC_Titus_armed_base: QIN_Titus_arx20_DES
 {
-	class Turrets;
-	class ViewOptics;
-	//class ViewGunner;
-	class CommanderTurret;
 	class ARX20Turret;
-	class CargoTurret_01;
-	class CargoTurret_02;
-	class AnimationSources;
-	class Components;
-	class Door_LF;
-	class Door_RF;
-	class Door_rear;
-	class comp_camonet;
-	class comp_klec;
-	class EventHandlers;
-	scope=0;
-	scopeCurator=0;
-	RC_Local=1; //1 = requires transfer of locality/ownership for full functionality
 };
 class RC_Titus_armed_D: RC_Titus_armed_base
 {
-	author="Ascent";
-	faction="RemoteControlled_B";
 	displayName="RC Titus 20mm";
-	editorSubcategory="RC_Titus_subcat";
-	scope=2;
-	scopeCurator=2;
-	//side=0;
-	forceInGarage=1;
-	vehicleClass="Autonomous";
-	uavCameraDriverPos="PiP0_pos";
-	uavCameraDriverDir="PiP0_dir";
-	isUav=1;
-	textPlural="UGVs";
-	textSingular="UGV";
-	crew="B_UAV_AI";
-	forceHideDriver=1;
-	driverForceOptics=1;
-	//memoryPointDriverOptics="optika_velitel";
-	driverCompartments="Compartment3";
-	//commanding=2;
-	crewCrashProtection=0.01;
-
-	uavCameraGunnerPos="PiP1_pos";
-	uavCameraGunnerDir="PiP1_dir";
-
-	class EventHandlers: EventHandlers
-	{
-		init="(_this select 0) spawn {waitUntil {!isNull commander _this}; _this deleteVehicleCrew commander _this;};";
-		#include "\Remote_Controlled_Artillery\includes\DriverControlsEH_ICV.hpp"
-	};
-
-	//uavCameraGunnerPos="PiP1_pos";
-	//uavCameraGunnerDir="PiP1_dir";
 
 	class Turrets: Turrets
 	{
 		class CommanderTurret : CommanderTurret
 		{
-			gunnerCompartments="Compartment4";
 			primaryGunner=1;
 			primaryObserver=0;
-			gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
-			//personTurretAction="";	//no effect
-			//forceHideGunner=1;	//makes view bug
-			//forceHideCommander=1;	//makes view bug
-			//gunnerOpticsModel="\A3\Weapons_F\Reticle\Optics_Commander_02_n_F.p3d";
-			//turretInfoType="RscWeaponRangeFinder";
-			//turretInfoType="";
-			gunnerForceOptics=1;
-			//commanding=3;
+
 		};
 		class ARX20Turret: ARX20Turret
 		{
 			primaryGunner=0;
 			primaryObserver=1;
 		};
-		class CargoTurret_01: CargoTurret_01 {};
-		class CargoTurret_02: CargoTurret_02 {};
-	};
-
-	class AnimationSources: AnimationSources
-	{
-		class Door_LF: Door_LF
-		{
-			animPeriod=0.5;
-		};
-		class Door_RF: Door_RF
-		{
-			animPeriod=0.5;
-		};
-		class Door_rear: Door_rear
-		{
-			animPeriod=0.8;
-		};
-		class comp_camonet: comp_camonet
-		{
-			initPhase=1;
-		};
-		class comp_klec: comp_klec
-		{
-			initPhase=1;
-		};
-	};
-
-	animationList[]=
-	{
-		"comp_klec",
-		0,
-		"comp_camonet",
-		0
 	};
 };
+*/
