2e04001189a906aef66289f1556322feba698c33
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2024-08-25 17:42:52 +0200

    includes

--- addons/Remote_Controlled_Artillery/cfgVehicles/ICV_IFV_3.hpp
+++ addons/Remote_Controlled_Artillery/cfgVehicles/ICV_IFV_3.hpp
@@ -31,6 +31,9 @@ class RC_ICV_IFV_3_A_Base: B_APC_Wheeled_03_cannon_F
 class RC_ICV_IFV_3_A: RC_ICV_IFV_3_A_Base
 {
 	#include "\Remote_Controlled_Artillery\includes\UserActions_TakeDriverControls.hpp"
+	#include "\Remote_Controlled_Artillery\includes\Systems.hpp"
+	#include "\Remote_Controlled_Artillery\includes\DriverComponents4km.hpp"
+
 	author="Ascent";
 	faction="RemoteControlled_B";
 	scope=0;
@@ -43,25 +46,7 @@ class RC_ICV_IFV_3_A: RC_ICV_IFV_3_A_Base
 	ejectDeadDriver=0;
 	ejectDeadCommander=0;
 	crewCrashProtection=0.01;
-	radartype=2;
-	reportOwnPosition=1;
-	receiveRemoteTargets=1;
-	reportRemoteTargets=1;
-	laserScanner=1;
-	lockDetectionSystem=4;
-	incomingMissileDetectionSystem=16;
-	soundLocked[]=
-	{
-		"\A3\Sounds_F\weapons\Rockets\locked_1",
-		1,
-		1
-	};
-	soundIncommingMissile[]=
-	{
-		"\A3\Sounds_F\vehicles\air\noises\alarm_locked_by_missile_4",
-		0.39810717,
-		1
-	};
+
 	maxSpeed=120;
 	normalSpeedForwardCoef=0.64;
 	enginePower=553;
@@ -92,117 +77,6 @@ class RC_ICV_IFV_3_A: RC_ICV_IFV_3_A_Base
 		};
 		
 		#include "\Remote_Controlled_Artillery\includes\hitWheels.hpp"
-	};
-
-	class Components: Components
-	{
-		class SensorsManagerComponent
-		{
-			class Components
-			{
-				class LaserSensorComponent: SensorTemplateLaser
-				{
-					class AirTarget
-					{
-						minRange=4000;
-						maxRange=4000;
-						objectDistanceLimitCoef=-1;
-						viewDistanceLimitCoef=-1;
-					};
-					class GroundTarget
-					{
-						minRange=4000;
-						maxRange=4000;
-						objectDistanceLimitCoef=-1;
-						viewDistanceLimitCoef=-1;
-					};
-				};
-				class DataLinkSensorComponent: SensorTemplateDataLink
-				{
-					typeRecognitionDistance=4000;
-
-					class AirTarget
-					{
-						minRange=4000;
-						maxRange=4000;
-						objectDistanceLimitCoef=-1;
-						viewDistanceLimitCoef=-1;
-					};
-					class GroundTarget
-					{
-						minRange=4000;
-						maxRange=4000;
-						objectDistanceLimitCoef=-1;
-						viewDistanceLimitCoef=-1;
-					};
-				};
-
-				#include "\Remote_Controlled_Artillery\includes\passiveRadar.hpp"
-			};
-		};
-
-		class VehicleSystemsDisplayManagerComponentRight: DefaultVehicleSystemsDisplayManagerRight
-		{
-			defaultDisplay="SensorDisplay";
-
-			class Components
-			{
-				class SensorDisplay
-				{
-					componentType="SensorsDisplayComponent";
-					range[]={4000,2000,1000,500};
-					resource="RscCustomInfoSensors";
-				};
-				class MinimapDisplay
-				{
-					componentType="MinimapDisplayComponent";
-					resource="RscCustomInfoMiniMap";
-				};
-				class EmptyDisplay
-				{
-					componentType="EmptyDisplayComponent";
-				};
-				/*
-				class MineDetectorDisplay
-				{
-					componentType="MineDetectorDisplayComponent";
-					range=50;
-					resource="RscCustomInfoMineDetect";
-				};
-				*/
-			};
-		};
-		class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
-		{
-			defaultDisplay="EmptyDisplayComponent";
-
-			class Components
-			{
-				class SensorDisplay
-				{
-					componentType="SensorsDisplayComponent";
-					range[]={4000,2000,1000,500};
-					resource="RscCustomInfoSensors";
-				};
-				class MinimapDisplay
-				{
-					componentType="MinimapDisplayComponent";
-					resource="RscCustomInfoMiniMap";
-				};
-				class EmptyDisplay
-				{
-					componentType="EmptyDisplayComponent";
-				};
-				/*
-				class MineDetectorDisplay
-				{
-					componentType="MineDetectorDisplayComponent";
-					range=50;
-					resource="RscCustomInfoMineDetect";
-				};
-				*/
-			};
-		};
 	};
 
 	class AnimationSources: AnimationSources
