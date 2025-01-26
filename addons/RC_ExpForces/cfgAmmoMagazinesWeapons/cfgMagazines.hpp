aac069028f8b4bec3cb367e7b1d9b507eec477ef
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-01-26 23:56:48 +0100

    30mm AAV

--- addons/RC_ExpForces/cfgAmmoMagazinesWeapons/cfgMagazines.hpp
+++ addons/RC_ExpForces/cfgAmmoMagazinesWeapons/cfgMagazines.hpp
@@ -1,3 +1,25 @@
+class EF_160Rnd_30mm_HE_shells_Tracer_Red;
+class RC_EF_120Rnd_30mm_HEAB_T_W: EF_160Rnd_30mm_HE_shells_Tracer_Red
+{
+	ammo="RC_EF_B_30mm_HEAB_T_W";
+	tracersEvery=1;
+	displayName="30mm HEAB-T";
+	displayNameShort="30mm HEAB-T";
+};
+class RC_EF_120Rnd_30mm_HEAB_T_R: RC_EF_120Rnd_30mm_HEAB_T_W
+{
+	ammo="RC_EF_B_30mm_HEAB_T_R";
+};
+class RC_EF_120Rnd_30mm_HEAB_T_G: RC_EF_120Rnd_30mm_HEAB_T_W
+{
+	ammo="RC_EF_B_30mm_HEAB_T_G";
+};
+class RC_EF_120Rnd_30mm_HEAB_T_Y: RC_EF_120Rnd_30mm_HEAB_T_W
+{
+	ammo="RC_EF_B_30mm_HEAB_T_Y";
+};
+
+
 class EF_120Rnd_50mm_HE_shells_Tracer_Red;
 class RC_EF_120Rnd_50mm_HEAB_T_W: EF_120Rnd_50mm_HE_shells_Tracer_Red
 {
@@ -17,6 +39,28 @@ class RC_EF_120Rnd_50mm_HEAB_T_G: RC_EF_120Rnd_50mm_HEAB_T_W
 class RC_EF_120Rnd_50mm_HEAB_T_Y: RC_EF_120Rnd_50mm_HEAB_T_W
 {
 	ammo="RC_EF_B_50mm_HEAB_T_Y";
+};
+
+
+class EF_55Rnd_30mm_APFSDS_shells_Tracer_Red;
+class RC_EF_80Rnd_30mm_APFSDS_T_W: EF_30Rnd_55mm_APFSDS_shells_Tracer_Red
+{
+	tracersEvery=1;
+	displayName="30mm APFSDS-T";
+	displayNameShort="30mm APFSDS-T";
+	ammo="RC_EF_B_30mm_APFSDS_T_W";
+};
+class RC_EF_80Rnd_30mm_APFSDS_T_R: RC_EF_80Rnd_30mm_APFSDS_T_W
+{
+	ammo="RC_EF_B_30mm_APFSDS_T_R";
+};
+class RC_EF_80Rnd_30mm_APFSDS_T_G: RC_EF_80Rnd_30mm_APFSDS_T_W
+{
+	ammo="RC_EF_B_30mm_APFSDS_T_G";
+};
+class RC_EF_80Rnd_30mm_APFSDS_T_Y: RC_EF_80Rnd_30mm_APFSDS_T_W
+{
+	ammo="RC_EF_B_30mm_APFSDS_T_Y";
 };
 
 
--- addons/RC_ExpForces/cfgAmmoMagazinesWeapons/cfgWeapons.hpp
+++ addons/RC_ExpForces/cfgAmmoMagazinesWeapons/cfgWeapons.hpp
@@ -1,3 +1,59 @@
+class EF_autocannon_30mm_AAV9;
+class RC_EF_autocannon_30mm_Base: EF_autocannon_30mm_AAV9
+{
+	class HE;
+	class AP;
+};
+class RC_EF_autocannon_30mm: RC_EF_autocannon_30mm_Base
+{
+	displayName="XM913";
+	magazineReloadTime=30;	//40
+	//canLock=2;
+	FCSZeroingDelay=0.5;
+	maxZeroing=4000;
+
+	ballisticsComputer="1 + 2 + 16";
+	cursorAim="mg";
+
+	class HE: HE
+	{
+		displayName="XM913";
+		magazineReloadTime=30;	//40
+		FCSZeroingDelay=0.5;
+		maxZeroing=4000;
+
+		magazines[]=
+		{
+			"RC_EF_120Rnd_30mm_HEAB_T_W",
+			"RC_EF_120Rnd_30mm_HEAB_T_R",
+			"RC_EF_120Rnd_30mm_HEAB_T_G",
+			"RC_EF_120Rnd_30mm_HEAB_T_Y"
+		};
+
+		ballisticsComputer="1 + 2 + 16";
+		cursorAim="mg";
+	};
+	class AP: AP
+	{
+		displayName="XM913";
+		magazineReloadTime=30;	//40
+		FCSZeroingDelay=0.5;
+		maxZeroing=4000;
+
+		magazines[]=
+		{
+			"RC_EF_80Rnd_30mm_APFSDS_T_W",
+			"RC_EF_80Rnd_30mm_APFSDS_T_R",
+			"RC_EF_80Rnd_30mm_APFSDS_T_G",
+			"RC_EF_80Rnd_30mm_APFSDS_T_Y"
+		};
+
+		ballisticsComputer="1 + 2 + 16";
+		cursorAim="mg";
+	};
+};
+
+
 class EF_autocannon_50mm_AAV9;
 class RC_EF_autocannon_50mm_Base: EF_autocannon_50mm_AAV9
 {
