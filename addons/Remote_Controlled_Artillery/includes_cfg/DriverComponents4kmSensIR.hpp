7f2baa8f6ff4d72dc352253272d2e6d449334058
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-05-11 16:29:18 +0200

    testing

---
+++ addons/Remote_Controlled_Artillery/includes_cfg/DriverComponents4kmSensIR.hpp
@@ -0,0 +1,133 @@
+class Components: Components
+{
+	class SensorsManagerComponent
+	{
+		class Components
+		{
+			class LaserSensorComponent: SensorTemplateLaser
+			{
+				class AirTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=-1;
+					viewDistanceLimitCoef=-1;
+				};
+				class GroundTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=-1;
+					viewDistanceLimitCoef=-1;
+				};
+			};
+			class DataLinkSensorComponent: SensorTemplateDataLink
+			{
+				typeRecognitionDistance=4000;
+
+				class AirTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=-1;
+					viewDistanceLimitCoef=-1;
+				};
+				class GroundTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=-1;
+					viewDistanceLimitCoef=-1;
+				};
+			};
+			class IRSensorComponent: SensorTemplateIR
+			{
+				typeRecognitionDistance=3000;
+
+				class AirTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=-1;
+					viewDistanceLimitCoef=-1;
+				};
+				class GroundTarget
+				{
+					minRange=4000;
+					maxRange=4000;
+					objectDistanceLimitCoef=1;
+					viewDistanceLimitCoef=1;
+				};
+				maxTrackableSpeed=600;
+				angleRangeHorizontal=0.5;
+				angleRangeVertical=0.5;
+				animDirection="mainGun";
+			};
+			#include "\Remote_Controlled_Artillery\includes_cfg\UAS_Sensor.hpp"
+			#include "\Remote_Controlled_Artillery\includes_cfg\passiveRadar.hpp"
+		};
+	};
+
+	class VehicleSystemsDisplayManagerComponentRight: DefaultVehicleSystemsDisplayManagerRight
+	{
+		defaultDisplay="SensorDisplay";
+
+		class Components
+		{
+			class SensorDisplay
+			{
+				componentType="SensorsDisplayComponent";
+				range[]={4000,2000,1000,400};
+				resource="RscCustomInfoSensors";
+			};
+			class MinimapDisplay
+			{
+				componentType="MinimapDisplayComponent";
+				resource="RscCustomInfoMiniMap";
+			};
+			class EmptyDisplay
+			{
+				componentType="EmptyDisplayComponent";
+			};
+			/*
+			class MineDetectorDisplay
+			{
+				componentType="MineDetectorDisplayComponent";
+				range=50;
+				resource="RscCustomInfoMineDetect";
+			};
+			*/
+		};
+	};
+	class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
+	{
+		defaultDisplay="SensorDisplay";
+
+		class Components
+		{
+			class SensorDisplay
+			{
+				componentType="SensorsDisplayComponent";
+				range[]={400,1000,2000,4000};
+				resource="RscCustomInfoSensors";
+			};
+			class MinimapDisplay
+			{
+				componentType="MinimapDisplayComponent";
+				resource="RscCustomInfoMiniMap";
+			};
+			class EmptyDisplay
+			{
+				componentType="EmptyDisplayComponent";
+			};
+			/*
+			class MineDetectorDisplay
+			{
+				componentType="MineDetectorDisplayComponent";
+				range=50;
+				resource="RscCustomInfoMineDetect";
+			};
+			*/
+		};
+	};
+};
