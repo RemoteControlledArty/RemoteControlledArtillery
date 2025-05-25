7f3f1977a205abed47a86d1cda5d9bacde271008
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-05-25 15:06:49 +0200

    rebalance

--- addons/RC_Titus/includes_vicmags/mags_Titus_HMG_green.hpp
+++ addons/RC_Titus/includes_vicmags/mags_Titus_HMG_green.hpp
@@ -2,8 +2,12 @@ magazines[]=
 {
 	"RC_150Rnd_127x99_T_G",
 	"RC_150Rnd_127x99_T_G",
+	"RC_150Rnd_127x99_T_G",
 	"RC_150Rnd_127x99_SLAP_T_G",
 	"RC_150Rnd_127x99_SLAP_T_G",
+	"RC_150Rnd_127x99_SLAP_T_G",
+	"RC_150Rnd_762x51_T_G",
+	"RC_150Rnd_762x51_T_G",
 	"RC_150Rnd_762x51_T_G",
 	"RC_150Rnd_762x51_T_G",
 	"RC_150Rnd_762x51_T_G",
