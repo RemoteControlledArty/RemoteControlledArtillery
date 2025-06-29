3b930a988e9ec573bde1734ae666702e9f650d90
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-06-29 15:44:05 +0200

    -

--- addons/Remote_Controlled_Artillery/cfgVehicles/MortarUGV.hpp
+++ addons/Remote_Controlled_Artillery/cfgVehicles/MortarUGV.hpp
@@ -24,11 +24,6 @@ class RC_Mortar_UGV_Base: RC_Mortar_UGV_Core
 {
 	class EventHandlers: EventHandlers
 	{
-		class RC_Detection
-		{
-			#include "\Remote_Controlled_Artillery\includes_script\AT_SourceIndicator.hpp"
-			#include "\Remote_Controlled_Artillery\includes_script\cUAS_Beep_400m.hpp"
-		};
 		class RC_LightsOff
 		{
 			#include "\Remote_Controlled_Artillery\includes_script\initLightsOff.hpp"
