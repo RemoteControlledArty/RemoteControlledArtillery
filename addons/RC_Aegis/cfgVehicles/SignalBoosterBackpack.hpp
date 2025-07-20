68b01c1c71f1c29e5ce22a2df12f60bea9f8701a
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-07-20 22:24:11 +0200

    -

--- addons/RC_Aegis/cfgVehicles/SignalBoosterBackpack.hpp
+++ addons/RC_Aegis/cfgVehicles/SignalBoosterBackpack.hpp
@@ -1,14 +1,14 @@
 class B_RadioBag_01_coyote_F;
 class RC_SignalBoosterLRR_a: B_RadioBag_01_coyote_F
 {
-	displayName="Crocus Signal Booster";
+	displayName="Crocus Signal Booster [DLC]";
 	mass=80;
 	maximumLoad=240;
 };
 class B_RadioBag_01_green_F;
 class RC_SignalBoosterLRR_g: B_RadioBag_01_green_F
 {
-	displayName="Crocus Signal Booster";
+	displayName="Crocus Signal Booster [DLC]";
 	mass=80;
 	maximumLoad=240;
 };
