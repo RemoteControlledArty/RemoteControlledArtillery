64f14c4a26e449fb9625e5e2aa63270c3ac20603
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-09-13 14:28:42 +0200

    -

--- addons/Remote_Controlled_Artillery/cfgVehicles/GPSTarget.hpp
+++ addons/Remote_Controlled_Artillery/cfgVehicles/GPSTarget.hpp
@@ -43,38 +43,48 @@ class RC_ArtySourceTarget: RC_GPSDatalinkTarget
 class RC_CRAM_Target_Base: All
 {
 	author="Ascent";
-	displayName="R/A/M Projectile";
-	scope=1;	//0?
-	scopeCurator=1;	//0?
+	displayName="RAM Projectile";
+	scope=0;
+	scopeCurator=0;
 	scopeArsenal=0;
 
 	alwaysTarget=1;
+	threat[]={0,1,1};
+	cost=10000000;
 	armor=1;
 	side=5;
-	destrType="DestructNo";
+	destrType="DestructDefault";	//destrType="DestructNo";
 	irTarget=1;
-	//sualTarget=1;
+	visualTarget=1;	//0
+	maxSpeed=1750;	//0
 
 	model="\A3\Weapons_f\laserTgt.p3d";
+	//model="\A3\Weapons_F_EPC\Ammo\Bomb_03_F.p3d";
+
 	reversed=0;
 	hasDriver=0;
 	icon="iconLaserTarget";
-	accuracy=9.9999997e-005;
+	accuracy=1000;		//9.9999997e-005
 	picture="pictureLaserTarget";
 	weapons[]={"FakeWeapon"};
 	magazines[]={"FakeWeapon"};
 	type=1;
-	threat[]={0,0,0};	//threat[]={1,1,1};
-	maxSpeed=1750;	//0
 	simulation="artillerymarker";	//forgot why, but its one of the few/only things that worked
 	laserTarget=0;
 	nvTarget=0;
 	artilleryTarget=0;	//1 would make it unlockable on datalink panel by all rounds
-	cost=2500000;	//makes it easier to select in datalink
 };
-class RC_CRAM_Target_Base: All
+class RC_CRAM_Target_B: RC_CRAM_Target_Base
 {
-	scope=1;	//0?
-	scopeCurator=1;	//0?
-
+	scope=1;
+	scopeCurator=1;
 	side=1;
+};
+class RC_CRAM_Target_O: RC_CRAM_Target_B
+{
+	side=0;
+};
+class RC_CRAM_Target_I: RC_CRAM_Target_B
+{
+	side=2;
+};
