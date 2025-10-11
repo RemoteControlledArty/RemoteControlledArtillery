1237bb1782bb0ec0ed5add3536e293e97ee083d1
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2025-10-11 05:55:03 +0200

    -

--- addons/RC_RHS_AFRF/includes_vicmags/mags_T90AM_com_red.hpp
+++ addons/RC_RHS_AFRF/includes_vicmags/mags_T90AM_com_red.hpp
@@ -6,7 +6,7 @@ magazines[]=
 	"RC_150Rnd_127x99_SLAP_T_R",
 	//"RC_150Rnd_127x99_SLAP_T_R",
 	
-	"RC_150Rnd_127x99_cUAS",
+	//"RC_150Rnd_127x99_cUAS",
 	
 	"Laserbatteries",
 	"SmokeLauncherMag",
--- addons/RC_RHS_AFRF/includes_vicmags/mags_T90AM_com_yellow.hpp
+++ addons/RC_RHS_AFRF/includes_vicmags/mags_T90AM_com_yellow.hpp
@@ -6,7 +6,7 @@ magazines[]=
 	"RC_150Rnd_127x99_SLAP_T_G",
 	//"RC_150Rnd_127x99_SLAP_T_G",
 	
-	"RC_150Rnd_127x99_cUAS",
+	//"RC_150Rnd_127x99_cUAS",
 	
 	"Laserbatteries",
 	"SmokeLauncherMag",
