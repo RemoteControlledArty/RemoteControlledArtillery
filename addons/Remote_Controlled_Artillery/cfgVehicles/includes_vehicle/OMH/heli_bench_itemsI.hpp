8b946534fe8f7039b1a754ba885c925f6470aa9a
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2026-03-29 16:53:47 +0200

    -

---
+++ addons/Remote_Controlled_Artillery/cfgVehicles/includes_vehicle/OMH/heli_bench_itemsI.hpp
@@ -0,0 +1,77 @@
+class TransportMagazines
+{
+	class _xx_HandGrenade
+	{
+		magazine="HandGrenade";
+		count=2;
+	};
+	class _xx_SmokeShell
+	{
+		magazine="SmokeShell";
+		count=2;
+	};
+	class _xx_Laserbatteries
+	{
+		magazine="Laserbatteries";
+		count=1;
+	};
+	class _xx_DemoCharge_Remote_Mag
+	{
+		magazine="DemoCharge_Remote_Mag";
+		count=2;
+	};
+};
+class TransportItems
+{
+	class _xx_I_UavTerminal
+	{
+		name="I_UavTerminal";
+		count=2;
+	};
+	class _xx_Toolkit
+	{
+		name="Toolkit";
+		count=2;
+	};
+	class _xx_Medikit
+	{
+		name="Medikit";
+		count=1;
+	};
+	class _xx_FirstAidKit
+	{
+		name="FirstAidKit";
+		count=1;
+	};
+	class _xx_MineDetector
+	{
+		name="MineDetector";
+		count=1;
+	};
+};
+class TransportWeapons
+{
+	class _xx_launch_NLAW_F
+	{
+		weapon="launch_NLAW_F";
+		count=1;
+	};
+	class _xx_Improved_FOV_Laserdesignator_A
+	{
+		weapon="Improved_FOV_Laserdesignator_A";
+		count=1;
+	};
+};
+class TransportBackpacks
+{
+	class _xx_B_Kitbag_rgr
+	{
+		backpack="B_Kitbag_rgr";
+		count=2;
+	};
+	class _xx_B_Parachute
+	{
+		backpack="RC_LightParachute";
+		count=11;
+	};
+};
