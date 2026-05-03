5c8e749fab44ca6bf40a0211b79d7a468752f68a
Apex <148592599+RemoteControlledArty@users.noreply.github.com>
2026-05-03 22:32:35 +0200

    -

--- addons/RC_60mmULM/cfgAmmoMagazinesWeapons/cfgMineTriggers.hpp
+++ addons/RC_60mmULM/cfgAmmoMagazinesWeapons/cfgMineTriggers.hpp
@@ -2,6 +2,6 @@ class RangeTriggerBounding;
 class RC_ULM_RangeTriggerBounding: RangeTriggerBounding
 {
 	mineTriggerRange=12;	//3		//12m is the same as 60mm HE indirectHit radius
-	mineTriggerActivationRange=20;	//5
-	restrictZoneRadius=20;	//5
+	mineTriggerActivationRange=16;	//5
+	restrictZoneRadius=16;	//5
 };
--- addons/Remote_Controlled_Artillery/cfgAmmoMagazinesWeapons/MineTriggers.hpp
+++ addons/Remote_Controlled_Artillery/cfgAmmoMagazinesWeapons/MineTriggers.hpp
@@ -1,6 +1,12 @@
 class RangeTriggerBounding;
 class RC_RangeTriggerBounding: RangeTriggerBounding
 {
+	mineTriggerRange=9;	//3
+	mineTriggerActivationRange=10;	//5
+	restrictZoneRadius=10;	//5
+};
+class RC_RangeTriggerBounding_60mm: RangeTriggerBounding
+{
 	mineTriggerRange=12;	//3
 	mineTriggerActivationRange=20;	//5
 	restrictZoneRadius=20;	//5
