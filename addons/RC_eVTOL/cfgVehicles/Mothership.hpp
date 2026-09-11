28f8ea6bce7116103ef66ac10b87907a4869d7f7
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2026-09-11 16:18:48 +0200

    Update Mothership configuration with new parameters
    Added camouflage and target size parameters to Mothership.

--- addons/RC_eVTOL/cfgVehicles/Mothership.hpp
+++ addons/RC_eVTOL/cfgVehicles/Mothership.hpp
@@ -168,6 +168,12 @@ class RC_MosquitoMothership_NoCam_Core: RC_MosquitoMothership_NoCam_Fetch
 			};
 		};
 	};
+
+    camouflage=75;				//150
+    //audible=?;                //?  
+	radarTargetSize=0.1;		//0.15
+	visualTargetSize=0.3;		//0.9
+	irTargetSize=0.3;			//0.8
 	
 	displayName="RC Mosquito Mothership [no camera]";
 	editorSubcategory="RC_eVTOL_subcat";
