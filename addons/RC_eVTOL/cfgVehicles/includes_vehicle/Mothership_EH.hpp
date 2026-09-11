da93f5120778c7bf7fc2180847906545c240373f
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2026-09-11 16:27:41 +0200

    Add event handlers for RC vehicle interactions

--- addons/RC_eVTOL/cfgVehicles/includes_vehicle/Mothership_EH.hpp
+++ addons/RC_eVTOL/cfgVehicles/includes_vehicle/Mothership_EH.hpp
@@ -1,1 +1,23 @@
+class EventHandlers: EventHandlers
+{
+	class RC_FlyInHeight
+	{
+		postInit="params ['_entity'];  _entity flyInHeight 4000;";	//flyInHeight = where local argument, global effect
+	};
+	class RC_Detach
+	{
+		deleted="if (!isserver) exitwith {}; \
+		params ['_entity']; \
+		private _attObj = (attachedObjects _entity); \
+		if (count _attObj > 0) then { \
+			{deleteVehicle _x} forEach _attObj; \
+		};";
 
+
+		killed="params ['_unit']; \
+		private _attObj = (attachedObjects _unit); \
+		if (count _attObj > 0) then { \
+			{_x disableCollisionWith _unit; detach _x} forEach _attObj; \
+		};";
+	};
+};
