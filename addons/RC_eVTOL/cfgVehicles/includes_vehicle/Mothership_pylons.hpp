cb51d827300a8d4a314410e58ae2f7afa904ed1f
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2026-09-11 16:42:21 +0200

    Add Mothership_pylons.hpp for transport pylons configuration

---
+++ addons/RC_eVTOL/cfgVehicles/includes_vehicle/Mothership_pylons.hpp
@@ -0,0 +1,17 @@
+class Components: Components
+{
+	class TransportPylonsComponent: TransportPylonsComponent
+	{
+		class pylons: pylons
+		{
+			class pylon1: pylon1
+			{
+				attachment="";
+			};
+   			class pylon2: pylon2
+			{
+				attachment="";
+			};
+		};
+	};
+};
