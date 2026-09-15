268628ff1dd77e4a5f5241157a04984aa52f9dcf
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2026-09-15 18:31:21 +0200

    Add configuration for AH-6M Little Bird AI vehicles

---
+++ addons/RC_AI/cfgVehicles/Heli_H6M.hpp
@@ -0,0 +1,64 @@
+class RC_MELB_AH6M_Base;
+class RC_AI_MELB_AH6M_Fetch: RC_MELB_AH6M_Base
+{
+	#include "\RC_RHS\cfgVehicles\includes_vehicle\H6M\H6M_fetch.hpp"
+};
+class RC_AI_MELB_AH6M_Core: RC_AI_MELB_AH6M_Fetch
+{
+	displayName="[AI] AH-6M - Little Bird";
+	editorSubcategory="RC_Heli_subcat";
+
+	class Components: Components
+	{
+		#include "\RC_RHS\cfgVehicles\includes_vehicle\H6M\H6M_4pylons_AI.hpp"
+	};
+
+    class HitPoints: HitPoints
+	{
+		/*
+		class HitFuel: HitFuel
+		{
+			armor=-150;				//V 1 = -40, H ?, RC -150
+			explosionShielding=1;	//V 1, RC 1
+		};
+		class HitAvionics: HitAvionics
+		{
+			armor=-80;					//V 2 = -80, H ?, RC -80
+			explosionShielding=1;		//V 1, RC 1
+		};
+		*/
+		class HitVRotor: HitVRotor
+		{
+			//armor=-200;				//V 3 = -120, H 0.054, 0.81 = -32.4, RC -200
+			explosionShielding=3;		//V 6, H 0.27, RC 1.5
+		};
+	};
+};
+class RC_AI_MELB_AH6M_Base: RC_AI_MELB_AH6M_Core
+{
+  class EventHandlers: EventHandlers
+	{
+		#include "\Remote_Controlled_Artillery\includes_script\getOutEngineOffEH.hpp"
+	};
+};
+
+
+class RC_AI_B_MELB_AH6M: RC_AI_MELB_AH6M_Base
+{
+	scope=2;
+	scopeCurator=2;
+	forceInGarage=1;
+
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\heli_sideB_manned.hpp"
+	#include "\RC_RHS\cfgVehicles\includes_vehicle\H6M\H6M_itemsB.hpp"
+};
+class RC_AI_O_MELB_AH6M: RC_AI_B_MELB_AH6M
+{
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\heli_sideO_manned.hpp"
+	#include "\RC_RHS\cfgVehicles\includes_vehicle\H6M\H6M_itemsO.hpp"
+};
+class RC_AI_I_MELB_AH6M: RC_AI_B_MELB_AH6M
+{
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\heli_sideI_manned.hpp"
+	#include "\RC_RHS\cfgVehicles\includes_vehicle\H6M\H6M_itemsI.hpp"
+};
