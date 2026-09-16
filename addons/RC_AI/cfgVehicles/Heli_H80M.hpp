8734fe1e85a3f7bc9960be3858c13813bfcde823
RC_Arty <148592599+RemoteControlledArty@users.noreply.github.com>
2026-09-16 15:22:15 +0200

    Add RC_AI_MH80M_Core class with pylons setup
    Added new RC_AI_MH80M_Core class with transport pylons configuration.

--- addons/RC_AI/cfgVehicles/Heli_H80M.hpp
+++ addons/RC_AI/cfgVehicles/Heli_H80M.hpp
@@ -16,7 +16,7 @@ class RC_AI_MH80M_DAP_Fetch: RC_MH80M_DAP_Base
 };
 class RC_AI_MH80M_DAP_Core: RC_AI_MH80M_DAP_Fetch
 {
-  	displayName="[AI] MH80M DAP - Ghost Hawk"
+  	displayName="[AI] MH80M DAP - Ghost Hawk";
 	editorSubcategory="RC_Heli_subcat";
 
 	class Components: Components
@@ -158,6 +158,134 @@ class RC_AI_O_MH80M_DAP: RC_AI_B_MH80M_DAP
 	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\H80M\H80M_itemsO.hpp"
 };
 class RC_AI_I_MH80M_DAP: RC_AI_B_MH80M_DAP
+{
+	#include "\RC_AI\includes_cfg\heli_sideI_manned.hpp"
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\H80M\H80M_itemsI.hpp"
+};
+
+
+class RC_AI_MH80M_Core: RC_AI_MH80M_DAP_Core
+{
+  	displayName="[AI] MH80M - Ghost Hawk"
+
+	class Components: Components
+	{
+		class TransportPylonsComponent: TransportPylonsComponent
+		{
+			class pylons: pylons
+			{
+				//left outer
+				class pylon1: pylon1
+				{
+					hardpoints[]=
+					{
+						"RC_Hardpoint_FuelTank",
+
+						"RC_AI_Hardpoint_V1",
+						"RC_AI_Hardpoint_V1_Left",
+						"RC_AI_Hardpoint_V2",
+						"RC_AI_Hardpoint_V2_Left",
+						"RC_AI_Hardpoint_V3",
+						"RC_AI_Hardpoint_V3_Left",
+						"RC_AI_Hardpoint_V4",
+						"RC_AI_Hardpoint_V4_Left"
+					};
+
+					attachment="RC_AI_PylonM_19Rnd_Hydra_MP_Proxy";
+				};
+				//left inner
+				class pylon2: pylon2
+				{
+					hardpoints[]=
+					{
+						"RC_Hardpoint_FuelTank",
+
+						"RC_AI_Hardpoint_V1",
+						"RC_AI_Hardpoint_V1_Left",
+						"RC_AI_Hardpoint_V2",
+						"RC_AI_Hardpoint_V2_Left",
+						"RC_AI_Hardpoint_V3",
+						"RC_AI_Hardpoint_V3_Left",
+						"RC_AI_Hardpoint_V4",
+						"RC_AI_Hardpoint_V4_Left"
+					};
+
+					attachment="RC_AI_PylonM_250Rnd_30mm_Proxy";
+				};
+				//right inner
+				class pylon3: pylon3
+				{
+					hardpoints[]=
+					{
+						"RC_Hardpoint_FuelTank",
+
+						"RC_AI_Hardpoint_V1",
+						"RC_AI_Hardpoint_V1_Right",
+						"RC_AI_Hardpoint_V2",
+						"RC_AI_Hardpoint_V2_Right",
+						"RC_AI_Hardpoint_V3",
+						"RC_AI_Hardpoint_V3_Right",
+						"RC_AI_Hardpoint_V4",
+						"RC_AI_Hardpoint_V4_Right"
+					};
+
+					attachment="RC_AI_PylonM_250Rnd_30mm_Proxy";
+				};
+				//right outer
+				class pylon4: pylon4
+				{
+					hardpoints[]=
+					{
+						"RC_Hardpoint_FuelTank",
+
+						"RC_AI_Hardpoint_V1",
+						"RC_AI_Hardpoint_V1_Right",
+						"RC_AI_Hardpoint_V2",
+						"RC_AI_Hardpoint_V2_Right",
+						"RC_AI_Hardpoint_V3",
+						"RC_AI_Hardpoint_V3_Right",
+						"RC_AI_Hardpoint_V4",
+						"RC_AI_Hardpoint_V4_Right"
+					};
+
+					attachment="RC_AI_PylonM_19Rnd_Hydra_MP_Proxy";
+				};
+				//center
+				class camera: camera
+				{
+				};
+				//left
+				class wingtip1: wingtip1
+				{
+				};
+				//right
+				class wingtip2: wingtip2
+				{
+				};
+			};
+		};
+	};
+};
+class RC_AI_MH80M_Base: RC_AI_MH80M_Core
+{
+};
+
+
+class RC_AI_B_MH80M: RC_AI_MH80M_Base
+{
+	scope=2;
+	scopeCurator=2;
+	forceInGarage=1;
+
+	#include "\RC_AI\includes_cfg\heli_sideB_manned.hpp"
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\H80M\H80M_itemsB.hpp"
+};
+class RC_AI_O_MH80M: RC_AI_B_MH80M
+{
+	#include "\RC_AI\includes_cfg\heli_sideO_manned.hpp"
+	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\H80M\H80M_itemsO.hpp"
+};
+class RC_AI_I_MH80M: RC_AI_B_MH80M
 {
 	#include "\RC_AI\includes_cfg\heli_sideI_manned.hpp"
 	#include "\Remote_Controlled_Artillery\cfgVehicles\includes_vehicle\H80M\H80M_itemsI.hpp"
